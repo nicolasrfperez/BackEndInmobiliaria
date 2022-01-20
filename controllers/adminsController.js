ca9625f77ea983aed2624c3e072c459bd90744a1
nicolasrfperez <nicolasrfperez@hotmail.com>
2022-01-20 18:36:54 -0300

    creacion de admin anda tarda una banda y la base da rejection id 1 o 2 pero carga, login si el usuario esta mal rechaza pero entra con cualquier password

--- controllers/adminsController.js
+++ controllers/adminsController.js
@@ -7,22 +7,12 @@ module.exports={
     getOne: function(req, res, next) {
         res.send('respond with a resource');
     },
-    create: function(req, res, next) {
-        //res.send('post users');
-        const admins = new adminsModel({
-            email : req.body.email,
-            user: req.body.user,
-            password: req.body.password
-        })
-        admins.save();
-        res.json(admins);
-    },
     validate: async (req, res, next) => {
         try{
             console.log(req.query)
-            const {error,message,userAdmin} = await adminsModel.validateUser(req.body.user,req.body.password);
+            const {error,message,admins} = await adminsModel.validateUser(req.body.user,req.body.password);
             if(!error){
-                const token = jwt.sign({userId:userAdmin._id},req.app.get("secretKey"),{expiresIn:"1h"});
+                const token = ({userId:admins._id});
                 res.json({message:message,token:token});
                 return;
             }
@@ -34,6 +24,18 @@ module.exports={
         }
         
     },
+    create:  function (req, res, next) {
+            console.log(req.body);
+            const admins =  new adminsModel({
+                email: req.body.email,
+                user:req.body.user,
+                password:req.body.password
+            })
+            admins.save();
+            res.json(admins)
+    },
+    
+
     modify:function(req, res, next) {
         console.log(`este es el id de put ${req.params.id}`)
         res.send('put users');
--- models/adminsModel.js
+++ models/adminsModel.js
@@ -1,4 +1,4 @@
-const mongoose = require('../bin/mongodb');
+/*const mongoose = require('../bin/mongodb');
 
 const adminsSchema = new mongoose.Schema ({
     email : "string",
@@ -6,4 +6,33 @@ const adminsSchema = new mongoose.Schema ({
     password: "string"
 })
 
-module.exports = mongoose.model("admins", adminsSchema);
+module.exports = mongoose.model("admins", adminsSchema);*/
+
+const mongoose = require("../bin/mongodb");
+
+
+const adminsSchema = new mongoose.Schema ({
+    email : String,
+    user: String,
+    password: String
+})/*
+adminsSchema.pre("save",function(next){
+    this.password = password;
+    next();
+
+})*/
+adminsSchema.statics.validateUser = async function(user,password){
+    const admins = await this.findOne({user:user});
+    if(admins){
+        if/*bcrypt.compareSync*/(password,admins.password){
+            //User y password ok, generar token
+            
+            return {error:false,message:"usuario ok",admins:admins.password};
+        }else{
+            return {error:true,message:"password incorrecto"};
+        }
+    }else{
+        return {error:true,message:"usuario incorrecto"};
+    }
+}
+module.exports = mongoose.model("admins",adminsSchema);
